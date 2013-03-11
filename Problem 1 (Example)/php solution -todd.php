80156f87bef8ab5aefc8c9681423230aa35b59ff
Todd Perry <up612136@myport.ac.uk>
2013-03-11 18:48:53 +0000

    Added PHP solution

---
+++ Problem 1 (Example)/php solution -todd.php
@@ -0,0 +1,17 @@
+<html>
+	<body>
+		<?php
+			// php solution
+			// problem 1
+			
+			$total = 0;
+			for ($i = 0; $i < 1000; $i++){
+				if ($i % 3 == 0 || $i % 5 == 0){
+					$total += $i;
+				}
+			}
+			
+			echo $i;
+		?>
+	</body>
+</html>
