15e128369f5e347b82f08301674e16867025c9e7
Todd Perry <up612136@myport.ac.uk>
2013-03-11 21:10:03 +0000

    Added Java Solution
    Added Java Solution
    Streamlines Haskel Code

--- Problem 1 (Example)/haskell solution -todd.hs
+++ Problem 1 (Example)/haskell solution -todd.hs
@@ -4,6 +4,5 @@
 prob1 :: [Int] -> Int
 prob1 [] = 0
 prob1 (x:xs)
-    |mod x 3 == 0 = prob1 xs + x
-    |mod x 5 == 0 = prob1 xs + x
+    |(mod x 3 == 0) || (mod x 5 == 0) = prob1 xs + x
     |otherwise = prob1 xs
