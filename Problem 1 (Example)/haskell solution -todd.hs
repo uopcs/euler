108adeeec7e5fdf9773df808a866ae19e89eb021
Todd Perry <up612136@myport.ac.uk>
2013-03-11 21:04:36 +0000

    Updated Haskell Formatting

--- Problem 1 (Example)/haskell solution -todd.hs
+++ Problem 1 (Example)/haskell solution -todd.hs
@@ -5,5 +5,5 @@ prob1 :: [Int] -> Int
 prob1 [] = 0
 prob1 (x:xs)
     |mod x 3 == 0 = prob1 xs + x
-	|mod x 5 == 0 = prob1 xs + x
-	|otherwise = prob1 xs
+    |mod x 5 == 0 = prob1 xs + x
+    |otherwise = prob1 xs
