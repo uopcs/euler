a0cdf9d9991c91783c25dc736465df5dfcd0747b
Todd Perry <up612136@myport.ac.uk>
2013-03-12 22:00:12 +0000

    Update My Haskell Solution
    (Just Checking If GUI git will push)

--- Problem 1 (Example)/haskell solution -todd.hs
+++ Problem 1 (Example)/haskell solution -todd.hs
@@ -1,8 +1,9 @@
 -- Haskell Solution
 
-
 prob1 :: [Int] -> Int
 prob1 [] = 0
 prob1 (x:xs)
     |(mod x 3 == 0) || (mod x 5 == 0) = prob1 xs + x
     |otherwise = prob1 xs
+	
+main = prob1 [0...999]
