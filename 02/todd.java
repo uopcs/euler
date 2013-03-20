95330b5f13ea040579cb823e3698e952de508b8a
Todd Perry <up612136@myport.ac.uk>
2013-03-20 23:28:54 +0000

    Update todd.java

--- 02/todd.java
+++ 02/todd.java
@@ -1,21 +1,15 @@
 int total = 0; // current total
-
-// loop through fibbonacci
-        
 int n2 = 1;
 int n1 = 1;
 int current = 2;
-        
 while (current < 4000000){
     //System.out.println(current);
     if (current % 2 == 0 && current < 4000000){
         // current is even
         total += current;
-    }
-    // increment
+    }// increment
     n2 = n1;
     n1 = current;
     current = n1 + n2;
-
 }
 return total;
